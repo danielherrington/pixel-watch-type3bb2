9983024ed31d3e1980057deb9a158770aa9b8075
Daniel Herrington <daniel@mac.lan>
2026-01-03 16:31:12 -0500

    Convert to full Android Studio Project structure

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.ressencebb2"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.ressencebb2"
+        minSdk = 33
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
